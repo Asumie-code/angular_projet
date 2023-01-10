637882852e0c5e4a8a5f98cfa3bdbf0d620bde16
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-01-10 20:37:52 +0100

    learn: component lifecycle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyComponent } from './components/myComponent/my-component.component';
+
 import { AutoMyComponentComponent } from './components/auto-my-component/auto-my-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent,
     AutoMyComponentComponent
   ],
   imports: [
--- src/app/components/auto-my-component/auto-my-component.component.ts
+++ src/app/components/auto-my-component/auto-my-component.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auto-my-component',
   templateUrl: './auto-my-component.component.html',
   styleUrls: ['./auto-my-component.component.css']
 })
-export class AutoMyComponentComponent {
+export class AutoMyComponentComponent implements OnInit {
+  
+  ngOnInit(): void {
+    console.log('called once on each initialization')  
+  }
+
+  
 
 }
