baa971aeb2996996412e00e5673801d230fea514
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-03-31 17:20:20 +0100

    learn:  ngModel
     - create greeting, contact, shared, customers
     - implement greeting module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
 
+
+// feature modules
+import { GreetingModule } from './greeting/greeting.module';
+
+// routing module
 import { AppRoutingModule } from './app-routing.module';
+// app root 
 import { AppComponent } from './app.component';
 
 
@@ -19,6 +25,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     FormsModule, // need this module to use [(ngModel)] directive,
     HttpClientModule,
+    GreetingModule.forRoot({userName: 'Miss Marple'})
   ],
   providers: [
   ],
