62aacdab5b57a879073d8a4fd98becdf7a21cf88
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-04-11 11:08:50 +0100

    learn: observables operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DelayedSquenceService } from './delayed-sequence.service';
+import { OperatorsService } from './observables-operators.service';
 
 
 @Component({
@@ -9,13 +10,22 @@ import { DelayedSquenceService } from './delayed-sequence.service';
 })
 export class AppComponent {
     sequence: number[] = []
-
-    constructor(public delayedSequence: DelayedSquenceService) {}
+    squaredNums: number[] = []
+    constructor(public delayedSequence: DelayedSquenceService, public squareNums: OperatorsService) {}
 
     getSequence() {
       this.delayedSequence.getDelayedSquence().subscribe((value) => {
         this.sequence.push(value)
       })
+    }
+
+    getSquaredNums() {
+      this.squareNums.squaredOddNums().subscribe({
+        next: (data) =>  { this.squaredNums.push(data as number) }, 
+        error: () =>  {console.log('something went wrong')} // will not run in case of error because the operator catched the error
+      })
+      
+
     }
     
 }
